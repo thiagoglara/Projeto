566ad5ff196a4ee47d8720a030bdb2f1b09bdb4f
thiagoglara <thiagoglara@gmail.com>
2025-08-04 14:56:03 -0300

    aula 8 adicionada

---
+++ aula8.py
@@ -0,0 +1,16 @@
+# if / elif    /else
+# se / se nao se  /se nao
+
+condicao1 = False
+condicao2 = False
+condicao3 = False
+condicao4 = False
+
+
+if condicao1:
+    print("este é o código do if")
+else:    
+    print("este é o else do primeiro if")
+if 10 == 10:
+    print("outro if")
+print("Fora do if")
